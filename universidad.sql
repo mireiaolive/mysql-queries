8b91028638dec442b3592ff0c12729384f494d28
Mireia Olivé <mirisfu@gmail.com>
2023-01-29 18:02:52 +0100

    commit

--- universidad.sql
+++ universidad.sql
@@ -149,19 +149,22 @@ AND c.anyo_fin IS NULL;
 
 -- Consultes resum:
 -- 1. Retorna el nombre total d'alumnes que hi ha.
-SELECT p.id, p.nombre 
+SELECT COUNT(p.id) AS alumnos
 FROM persona p
-WHERE tipo="alumno";
+WHERE tipo = 'alumno';
 
 -- 2. Calcula quants alumnes van néixer en 1999.
-SELECT p.id, p.nombre 
+SELECT COUNT(p.id) AS alumnos
 FROM persona p
-WHERE tipo="alumno"
+WHERE tipo='alumno'
 AND YEAR(fecha_nacimiento)=1999;
 
 -- 3. Calcula quants professors/es hi ha en cada departament. 
 -- El resultat només ha de mostrar dues columnes, una amb el nom del departament i una altra amb el nombre de professors/es que hi ha en aquest departament. 
 -- El resultat només ha d'incloure els departaments que tenen professors/es associats i haurà d'estar ordenat de major a menor pel nombre de professors/es.
+SELECT d.nombre AS departamento, p.nombre AS profesoras
+FROM persona p
+LEFT JOIN 
 
 -- 4. Retorna un llistat amb tots els departaments i el nombre de professors/es que hi ha en cadascun d'ells. 
 -- Tingui en compte que poden existir departaments que no tenen professors/es associats. Aquests departaments també han d'aparèixer en el llistat.
