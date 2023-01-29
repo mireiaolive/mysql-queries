37a6e14aca12cc6e99dec6ed45817d5459f61dfa
Mireia Olivé <mirisfu@gmail.com>
2023-01-29 19:50:24 +0100

    commit

--- universidad.sql
+++ universidad.sql
@@ -185,10 +185,26 @@ GROUP BY departamento;
 -- 5. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. 
 -- Tingues en compte que poden existir graus que no tenen assignatures associades. 
 -- Aquests graus també han d'aparèixer en el llistat. El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures.
+SELECT g.id, g.nombre AS grado, COUNT(a.id) AS asignatura
+FROM grado g
+LEFT JOIN asignatura a
+ON a.id_grado=g.id
+GROUP BY g.id
+ORDER BY asignatura DESC;
 
--- 6. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades.
+-- 6. Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, 
+-- dels graus que tinguin més de 40 assignatures associades.
+SELECT g.id, g.nombre AS grado, COUNT(a.id) AS asignatura
+FROM grado g
+LEFT JOIN asignatura a
+ON a.id_grado=g.id
+GROUP BY g.id
+HAVING COUNT(a.id) >= 40;
 
--- 7. Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus.
+
+-- 7. Retorna un llistat que mostri el nom dels graus i la suma del nombre total de crèdits que hi ha per a cada tipus d'assignatura. 
+-- El resultat ha de tenir tres columnes: nom del grau, tipus d'assignatura i la suma dels crèdits de totes les assignatures que hi ha d'aquest tipus.
+
 -- 8. Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats.
 -- 9. Retorna un llistat amb el nombre d'assignatures que imparteix cada professor/a. El llistat ha de tenir en compte aquells professors/es que no imparteixen cap assignatura. El resultat mostrarà cinc columnes: id, nom, primer cognom, segon cognom i nombre d'assignatures. El resultat estarà ordenat de major a menor pel nombre d'assignatures.
 -- 10. Retorna totes les dades de l'alumne/a més jove.
